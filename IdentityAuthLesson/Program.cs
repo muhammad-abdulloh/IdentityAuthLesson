cf8197c17a51b5abea7e58ed207a9db7b0bfe1de
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-22 12:11:07 +0500

    add authentication tugmacha

--- IdentityAuthLesson/Program.cs
+++ IdentityAuthLesson/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace IdentityAuthLesson
@@ -60,7 +61,34 @@ namespace IdentityAuthLesson
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
 
 
 
@@ -82,7 +110,7 @@ namespace IdentityAuthLesson
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            
             app.MapIdentityApi<IdentityUser>();
             app.MapGet("welcome", () => "Salom bolajonim")
                 .RequireAuthorization();
